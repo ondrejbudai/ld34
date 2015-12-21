76b2f2b4346143fa269ffc70ed0bae5abed0e18a
Ondřej Budai <ondrej@budai.cz>
2015-12-21 10:24:50 +0100

    * Better archive name of deployed archive

--- Travis-deploy.sh
+++ Travis-deploy.sh
@@ -8,17 +8,17 @@ chmod 0600 traviskey
 case "${os}" in
   linux32)
 	sftp -oStrictHostKeyChecking=no -i traviskey  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}-linux32.zip
+put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/SpacePilot-linux32-build${TRAVIS_BUILD_NUMBER}.zip
 EOF
     ;;
   linux64)
 	sftp -oStrictHostKeyChecking=no -i traviskey  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}-linux64.zip
+put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/SpacePilot-linux64-build${TRAVIS_BUILD_NUMBER}.zip
 EOF
     ;;
   win32)
 	sftp -oStrictHostKeyChecking=no -i traviskey  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}-win32.zip
+put ${PWD}/SpacePilot.zip /mnt/home/ondrej.budai/builds/SpacePilot-win32-build${TRAVIS_BUILD_NUMBER}.zip
 EOF
     ;;
 esac
