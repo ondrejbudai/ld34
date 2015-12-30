8168e35b7b885cba38bbee4807a6466848834821
Ondřej Budai <ondrej@budai.cz>
2015-12-30 14:12:28 +0100

    More adjustments

--- Travis-script.sh
+++ Travis-script.sh
@@ -5,18 +5,18 @@ set -x
 
 case "${os}" in
   linux32)
-    make clean
+    make target="linux32" clean
     make -j2 CXX="g++-5" target="linux32" release
-    make -j2 CXX="g++-5" zip
+    make -j2 CXX="g++-5" target="linux32" zip
     ;;
   linux64)
-    make clean
+    make target="linux64" clean
     make -j2 CXX="g++-5" target="linux64" release
-    make -j2 CXX="g++-5" zip
+    make -j2 CXX="g++-5" target="linux64" zip
     ;;
   win32)
-    make clean
+    make target="win32" clean
     make -j2 CXX="i686-w64-mingw32-g++-posix" target="win32" release
-    make -j2 CXX="i686-w64-mingw32-g++-posix" zip
+    make -j2 CXX="i686-w64-mingw32-g++-posix" target="win32" zip
     ;;
 esac
