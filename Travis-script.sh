3e3b1e99ed0c464fc0283d65444312797f18c204
Ondřej Budai <ondrej@budai.cz>
2016-02-02 22:00:01 +0100

    Fix compilers & one dumb error

--- Travis-script.sh
+++ Travis-script.sh
@@ -5,16 +5,17 @@ set -x
 
 case "${os}" in
   linux32)
-    cmake -DOPT:STRING=release -DTARGET:STRING=linux32 .
+    cmake -DOPT:STRING=release -DTARGET:STRING=linux32 -DCMAKE_CXX_COMPILER=g++-5 .
     make clean
     make zip
     ;;
   linux64)
-    cmake -DOPT:STRING=release -DTARGET:STRING=linux64 .
+    cmake -DOPT:STRING=release -DTARGET:STRING=linux64 -DCMAKE_CXX_COMPILER=g++-5 .
     make clean
     make zip
     ;;
   win32)
+    cmake -DOPT:STRING=release -DTARGET:STRING=win32 -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++-posix .
     make clean
     make zip
     ;;
