e1d39b19ee259ab01ee40ccf5ecd5b163657ffa1
Ondřej Budai <ondrej@budai.cz>
2016-02-02 21:49:27 +0100

    Travis, come to life! (probably very broken)

--- Travis-script.sh
+++ Travis-script.sh
@@ -5,18 +5,17 @@ set -x
 
 case "${os}" in
   linux32)
-    make target="linux32" clean
-    make -j2 CXX="g++-5" target="linux32" release
-    make -j2 CXX="g++-5" target="linux32" zip
+    cmake -DOPT:STRING=release -DTARGET:STRING=linux32 .
+    make clean
+    make zip
     ;;
   linux64)
-    make target="linux64" clean
-    make -j2 CXX="g++-5" target="linux64" release
-    make -j2 CXX="g++-5" target="linux64" zip
+    cmake -DOPT:STRING=release -DTARGET:STRING=linux64 .
+    make clean
+    make zip
     ;;
   win32)
-    make target="win32" clean
-    make -j2 CXX="i686-w64-mingw32-g++-posix" target="win32" release
-    make -j2 CXX="i686-w64-mingw32-g++-posix" target="win32" zip
+    make clean
+    make zip
     ;;
 esac
---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s . SpacePilot 2>/dev/null || true
+rm SpacePilot.zip 2>/dev/null || true
+if [ $# -ge 1 ] && [ "$1" -eq "windows" ]; then
+	zip SpacePilot.zip SpacePilot/spacepilot.exe SpacePilot/img/*.png SpacePilot/SDL2.dll SpacePilot/SDL2_image.dll SpacePilot/zlib1.dll SpacePilot/libpng16-16.dll SpacePilot/README SpacePilot/LICENSE
+else
+	zip SpacePilot.zip SpacePilot/spacepilot SpacePilot/img/*.png SpacePilot/README SpacePilot/LICENSE
+fi
