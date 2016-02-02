90d9977b4b3401c5ef7cd259550cc412f2116c9f
Ondřej Budai <ondrej@budai.cz>
2016-02-02 22:38:56 +0100

    Fixb

--- Travis-script.sh
+++ Travis-script.sh
@@ -7,16 +7,16 @@ case "${os}" in
   linux32)
     cmake -DOPT:STRING=release -DTARGET:STRING=linux32 -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_C_COMPILER=gcc-5 .
     make clean
-    make zip
+    make VERBOSE=1 zip
     ;;
   linux64)
     cmake -DOPT:STRING=release -DTARGET:STRING=linux64 -DCMAKE_CXX_COMPILER=g++-5 .
     make clean
-    make zip
+    make VERBOSE=1 zip
     ;;
   win32)
-    cmake -DOPT:STRING=release -DTARGET:STRING=win32 -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++-posix .
+    cmake -DOPT:STRING=release -DTARGET:STRING=win32 -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++-posix -DCMAKE_SYSTEM_NAME=Windows .
     make clean
-    make zip
+    make VERBOSE=1 zip
     ;;
 esac
