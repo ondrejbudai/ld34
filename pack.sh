646629ba1fe9efaa65413310f5debfcc94e65085
Ondřej Budai <ondrej@budai.cz>
2016-02-06 19:32:47 +0100

    Fix packing

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 
 ln -s . SpacePilot 2>/dev/null || true
 rm SpacePilot.zip 2>/dev/null || true
-if [ $# -ge 1 ] && [ "$1" -eq "windows" ]; then
+if [ $# -ge 1 ] && [ "$1" ==  "windows" ]; then
 	zip SpacePilot.zip SpacePilot/spacepilot.exe SpacePilot/img/*.png SpacePilot/SDL2.dll SpacePilot/SDL2_image.dll SpacePilot/zlib1.dll SpacePilot/libpng16-16.dll SpacePilot/README SpacePilot/LICENSE
 else
 	zip SpacePilot.zip SpacePilot/spacepilot SpacePilot/img/*.png SpacePilot/README SpacePilot/LICENSE
